0f95cfdfb94d4c88f6b7912f5698896cbb0b41b2
Raji Ross <grumpysucks@gmail.com>
2026-09-23 10:43:44 -0700

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        grids = [[set(),set(),set()],[set(),set(),set()],[set(),set(),set()]]
+        columns = [set(), set(), set(), set(), set(),set(),set(),set(),set()]
+        
+        for i, row in enumerate(board):
+            seen = set()
+            for j, num in enumerate(row):
+                if num.isdigit() and num in columns[j]:
+                    return False
+                elif num.isdigit():
+                    columns[j].add(num)
+                if num.isdigit() and num in seen:
+                    return False
+                if num.isdigit():
+                    seen.add(num)
+                if num.isdigit() and num in grids[i//3][j//3]:
+                    return False
+                elif num.isdigit():
+                    grids[i//3][j//3].add(num)
+        return True
